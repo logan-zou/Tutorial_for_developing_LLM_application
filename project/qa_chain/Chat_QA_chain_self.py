e7bc08d082c4ed4264f2d0d57835c7a539717a5a
xuhu0115 <64852985+xuhu0115@users.noreply.github.com>
2023-10-20 00:58:44 +0800

    修改qa_chain的逻辑

--- project/qa_chain/Chat_QA_chain_self.py
+++ project/qa_chain/Chat_QA_chain_self.py
@@ -9,12 +9,52 @@ import model_to_llm
 import get_vectordb
 
 
-class Chat_QA_chain_self():
+class Chat_QA_chain_self:
     """"
     带历史记录的问答链    
     """
-       
-    def answer(self, model:str, top_k, question:str=None, chat_history:list=[], file_path:str=None, persist_path:str=None,api_key: str = None, embedding = "openai"):
+    def __init__(self,model:str, top_k, chat_history:list=[], history_len:int=0, file_path:str=None, persist_path:str=None, api_key: str = None, embedding = "openai"):
+        self.model = model
+        self.top_k = top_k
+        self.chat_history = chat_history
+        self.history_len = history_len
+        self.file_path = file_path
+        self.persist_path = persist_path
+        self.api_key = api_key
+        self.embedding = embedding
+
+        self.vectordb = get_vectordb(self.file_path, self.persist_path, self.api_key, self.embedding)
+        self.llm = model_to_llm(self.model)
+
+        self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+        self.retriever = self.vectordb.as_retriever(search_type="similarity",   
+                                        search_kwargs={'k': self.top_k})  #默认similarity，k=4
+
+        self.qa = ConversationalRetrievalChain.from_llm(
+            llm = self.llm,
+            retriever = self.retriever,
+            memory = self.memory
+        )
+    
+    def clear_history(self):
+        "清空历史记录"
+        return self.chat_history.clear()
+
+    
+    def change_history_length(self):
+        """
+        保存指定对话轮次的历史记录
+        输入参数：
+        -history_len ：控制保留的最近 history_len 次对话
+        - chat_history：当前的历史对话记录
+        输出：返回最近 history_len 次对话
+        """
+        n = len(self.chat_history)
+        return self.chat_history[n-self.history_len:n]
+
+ 
+    def answer(self, question:str=None):
         """"
         核心方法，调用问答链
         arguments: 
@@ -28,45 +68,16 @@ class Chat_QA_chain_self():
         - embeddings：
         """
         if len(question) == 0:
-            return "", chat_history
+            return "", self.chat_history
 
-        
-        vectordb = get_vectordb(file_path, persist_path, api_key, embedding)
-        llm = model_to_llm(model)
+        result = self.qa({"question": question})     
+        self.chat_history.append((question,result['answer'])) #更新历史记录
 
-        memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-        retriever=vectordb.as_retriever(search_type="similarity",   
-                                        search_kwargs={'k': top_k})  #默认similarity，k=4
-
-        qa = ConversationalRetrievalChain.from_llm(
-            llm=llm,
-            retriever=retriever,
-            memory=memory
-        )
-
-        result = qa({"question": question})     
-        chat_history.append((question,result['answer'])) #更新历史记录
-
-        return result['answer'],chat_history  #返回本次回答和更新后的历史记录
+        return result['answer'],self.chat_history  #返回本次回答和更新后的历史记录
 
 
-
-    def clear_history(chat_history:list=[]):
-        "清空历史记录"
-        return chat_history.clear()
 
     
-    def change_history_length(history_len:int, chat_history:list=[]):
-        """
-        保存指定对话轮次的历史记录
-        输入参数：
-        -history_len ：控制保留的最近 history_len 次对话
-        - chat_history：当前的历史对话记录
-        输出：返回最近 history_len 次对话
-        """
-        n = len(chat_history)
-        return chat_history[n-history_len:n]
         
         
 
--- project/qa_chain/QA_chain_self.py
+++ project/qa_chain/QA_chain_self.py
@@ -7,13 +7,11 @@ import get_vectordb
 
 
 class QA_chain_self():
-    # 自定义 QA 链
-    
     """"
-    不带历史记录的问答链    
+    不带历史记录的问答链
+    - model：调用的模型名称
+    - template：可以自定义提示模板，没有输入则使用默认的提示模板default_template_rq    
     """
-
-    
 
     #基于召回结果和 query 结合起来构建的 prompt使用的默认提示模版
     default_template_rq = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
@@ -22,34 +20,37 @@ class QA_chain_self():
     问题: {question}
     有用的回答:"""
 
+    def __init__(self, model:str, file_path:str=None, persist_path:str=None, api_key: str = None, embedding = "openai", template=default_template_rq):
+        self.model = model
+        self.file_path = file_path
+        self.persist_path = persist_path
+        self.api_key = api_key
+        self.embedding = embedding
+        self.template = template
+        
+        self.vectordb = get_vectordb(self.file_path, self.persist_path, self.api_key, self.embedding)
+        self.llm = model_to_llm(self.model)
+
+        self.QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],
+                                    template=self.template)
+        # 自定义 QA 链
+        qa_chain = RetrievalQA.from_chain_type(llm=self.llm,
+                                        retriever=self.vectordb.as_retriever(),
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt":self.QA_CHAIN_PROMPT})
+
     #基于大模型的问答 prompt 使用的默认提示模版
     #default_template_llm = """请回答下列问题:{question}"""
            
-    
-
-    def answer(self, model:str, question:str=None, template=default_template_rq, file_path:str=None, persist_path:str=None,api_key: str = None, embedding = "openai"):
+    def answer(self, question:str=None):
         """"
         核心方法，调用问答链
         arguments: 
         - question：用户提问
-        - model：调用的模型名称
-        - top_k：返回检索的前k个相似文档
-        - template：可以自定义提示模板，没有输入则使用默认的提示模板default_template_rq
         """
 
         if len(question) == 0:
             return ""
         
-        vectordb = get_vectordb(file_path, persist_path, api_key, embedding)
-        llm = model_to_llm(model)
-        
-        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context","question"],
-                                    template=template)
-        # 自定义 QA 链
-        qa_chain = RetrievalQA.from_chain_type(llm=llm,
-                                        retriever=vectordb.as_retriever(),
-                                        return_source_documents=True,
-                                        chain_type_kwargs={"prompt":QA_CHAIN_PROMPT})
-
-        result = qa_chain({"query": question})
+        result = self.qa_chain({"query": question})
         return result["result"]   
